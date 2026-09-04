265b5b06f57802d6e79da43f2855f1e2d17d7192
Bhavya Sharma <bhavayavashisth@gmail.cp>
2026-09-04 22:02:42 +0530

    Added scan for the files

---
+++ internal/scanner/scanner.go
@@ -0,0 +1,62 @@
+// internal/scanner/scanner.go
+package scanner
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type FileInfo struct {
+	Path    string
+	Name    string
+	Ext     string
+	Size    int64
+	ModTime int64
+	IsDir   bool
+}
+
+// Scan reads files in a directory, non-recursive
+func Scan(dir string) ([]FileInfo, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []FileInfo
+
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			continue // skip files we can't read
+		}
+
+		// get extension (lowercase for consistency)
+		ext := strings.ToLower(filepath.Ext(entry.Name()))
+
+		files = append(files, FileInfo{
+			Path:    filepath.Join(dir, entry.Name()),
+			Name:    entry.Name(),
+			Ext:     ext,
+			Size:    info.Size(),
+			ModTime: info.ModTime().Unix(),
+			IsDir:   entry.IsDir(),
+		})
+	}
+
+	return files, nil
+}
+
+// GetStats returns basic stats about scanned files
+func GetStats(files []FileInfo) (total int, dirs int, filesCount int) {
+	total = len(files)
+	for _, f := range files {
+		if f.IsDir {
+			dirs++
+		} else {
+			filesCount++
+		}
+	}
+	return
+}
